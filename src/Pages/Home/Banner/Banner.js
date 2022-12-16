b8853f08699e1d7ef20a25040f1dca1717bc9d11
YasinSajeeb <yeasinalhossen@gmail.com>
2022-12-17 00:36:57 +0600

    counter commit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -7,8 +7,8 @@ const Banner = () => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Wanna have a best travel experience?</h1>
-      <p className="mb-5">We are offering best safe and enjoyable travel services to you.</p>
+      <h1 className="mb-5 text-5xl font-bold text-white">Wanna have a best travel experience?</h1>
+      <p className="mb-5 text-white">We are offering best safe and enjoyable travel services to you.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>

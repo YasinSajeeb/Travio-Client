b08f0ba37b1aad51eb86f28a7f7fcfec82e45d70
YasinSajeeb <yeasinalhossen@gmail.com>
2023-01-06 18:56:01 +0600

    react photo view

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServiceCard = ({service}) => {
     const {img, price, title, description} = service;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={img} alt="Shoes" />
+        <PhotoProvider>
+          <PhotoView src={img}>
+          <img src={img} alt="Shoes" />
+          </PhotoView>
+        </PhotoProvider>
+        
       </figure>
       <div className="card-body">
         <h2 className="card-title text-3xl text-blue-600 font-bold">{title}</h2>
         <p>{description}</p>
         <h5 className="text-xl font-bold">Price: <span className="text-xl text-blue-500">{price}</span> tk</h5>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">Details</button>
         </div>
       </div>
     </div>

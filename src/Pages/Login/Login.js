b40d72cef92ca2148a2477633dc3deed30738c3b
YasinSajeeb <yeasinalhossen@gmail.com>
2023-01-06 20:33:30 +0600

    dot env

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+    }
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Book a ticket now! And join the tour. Please Login to book a ticket.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+                <input onSubmit={handleLogin} className="btn btn-primary" type="submit" value="Login" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
